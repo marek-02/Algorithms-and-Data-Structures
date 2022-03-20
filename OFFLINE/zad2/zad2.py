1e65b98d069e9537306e754610a7255e409894bb
marek-02 <marekzuwala@gmail.com>
2022-03-20 08:42:36 +0100

    Optimalization

--- OFFLINE/zad2/zad2.py
+++ OFFLINE/zad2/zad2.py
@@ -1,6 +1,6 @@
 from zad2testy import runtests
 
-# def qSort(L, lewy, prawy):
+# def qSort1(L, lewy, prawy): #~2.75
 #     srodek = (lewy + prawy) // 2
 #     piwot = L[srodek]
 #     piwot = L[srodek]
@@ -17,33 +17,64 @@ from zad2testy import runtests
 #     if lewy < j - 1: qSort(L, lewy, j - 1)
 #     if prawy > j + 1: qSort(L, j + 1, prawy)
 
+# def qSor(A): #~2.78
+#     l = len(A)
+#     S = [(-1, -1) for i in range(l)]
+#     S[0] = (0, l - 1)
+#     ptr = 0
+
+#     while ptr >= 0:
+#         p, r = S[ptr]
+#         ptr -= 1
+#         q = partition(A, p, r)
+
+#         if q - 1 > p:
+#             ptr += 1
+#             S[ptr] = (p, q - 1)
+        
+#         if q + 1 < r:
+#             ptr += 1
+#             S[ptr] = (q + 1, r)
+
 
 def qSort(A, p, r):
-    # if p < r:
+    # if p < r: #~2.72
     #     q = partition(A, p, r)
     #     qSort(A, p, q - 1)
     #     qSort(A, q + 1, r)
-    while p < r:    
+    while p < r: #~2.73 (zoptymalizowana pamięć)
         q = partition(A, p, r)
-        qSort(A, p, q - 1)
-        p = q + 1
+        if q - p < r - q:
+            qSort(A, p, q - 1)
+            p = q + 1
+        else:
+            qSort(A, q + 1, r)
+            r = q - 1
         
 def partition(A, p, r):
     pivot = A[r]
-    
     i = p - 1
     for j in range(p, r):
         if A[j][0] < pivot[0] or (A[j][0] == pivot[0] and A[j][1] > pivot[1]):
             i += 1
-            A[i], A[j] = A[j], A[i]
-    A[i + 1], A[r] = A[r], A[i + 1]
-    return i + 1
+            tmp = A[i]
+            A[i] = A[j]
+            A[j] = tmp
+            #A[i], A[j] = A[j], A[i]
+    i += 1
+    tmp = A[i]
+    A[i] = A[r]
+    A[r] = tmp
+    return i 
+    #A[i + 1], A[r] = A[r], A[i + 1]
 
 
 def depth(L):
     l = len(L)
+    #qSor(L)
     qSort(L, 0, l - 1)
-    # return 0
+    #qSort1(L, 0, l - 1)
+    #return 0
     G = []
     maxGora = L[0][1]
     G.append([maxGora, 0])
@@ -59,7 +90,7 @@ def depth(L):
             G[g - 1][1] += 1
             j = g - 2
             while j >= S:
-                if G[j][1] < G[j + 1][1]:
+                if G[S][1] <= G[j + 1][1]:
                     S = j + 1
                     break
                 if L[i][1] > G[j][0]:
